a175acba51bdb614728978b74ecd9557f606a3c2
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-05-11 16:48:19 -0700

    reformatted js and css for improved legibility.

--- styles/sfhover.js
+++ styles/sfhover.js
@@ -1,22 +1,17 @@
-if (window.attachEvent)
-{
-  var sfHover = function() 
-  {
+if (window.attachEvent) {
+  var sfHover = function () {
     var sfEls = document.getElementById("main_nav").getElementsByTagName("LI");
-    for (var i = 0; i < sfEls.length; i++) 
-    {
-      sfEls[i].onmouseover = function() 
-      {
+    for (var i = 0; i < sfEls.length; i++) {
+      sfEls[i].onmouseover = function () {
         this.className += " sfhover";
       }
-	
-      sfEls[i].onmouseout = function() 
-      {
+
+      sfEls[i].onmouseout = function () {
         this.className = this.className.replace(new RegExp(" sfhover\\b"), "");
       }
     }
   }
 
-  window.attachEvent("onload", sfHover);		
+  window.attachEvent("onload", sfHover);
 }
 
--- styles/woodblock.js
+++ styles/woodblock.js
@@ -1,14 +1,14 @@
 /* NIFTY CONFIG */
 
-window.onload=function(){
-if(!NiftyCheck())
+window.onload = function () {
+  if (!NiftyCheck())
     return;
-Rounded("div.nifty1","all","transparent","#F0F0CE","border #006");
-/*Rounded("div.nifty1","all","#EAD893","transparent","border #800");*/
-Rounded("div.nifty2","all","transparent","#F2F8F8","border #9EB8B8");
-Rounded("div.nifty3","all","transparent","#FFF","border #FCAFA0");
-Rounded("div.nifty4","all","transparent","#E9E9E9","border #CDCDCD");
-Rounded("div.nifty5","all","transparent","#FEF4E0","border #FCAFA0");
-Rounded("div.nifty6","all","transparent","#FFF","border #DBD9D5");
-Rounded("div.nifty7","all","transparent","#FFF","border #006");
+  Rounded("div.nifty1", "all", "transparent", "#F0F0CE", "border #006");
+  /*Rounded("div.nifty1","all","#EAD893","transparent","border #800");*/
+  Rounded("div.nifty2", "all", "transparent", "#F2F8F8", "border #9EB8B8");
+  Rounded("div.nifty3", "all", "transparent", "#FFF", "border #FCAFA0");
+  Rounded("div.nifty4", "all", "transparent", "#E9E9E9", "border #CDCDCD");
+  Rounded("div.nifty5", "all", "transparent", "#FEF4E0", "border #FCAFA0");
+  Rounded("div.nifty6", "all", "transparent", "#FFF", "border #DBD9D5");
+  Rounded("div.nifty7", "all", "transparent", "#FFF", "border #006");
 }
